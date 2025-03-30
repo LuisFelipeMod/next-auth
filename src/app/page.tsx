d1396f9cc35732e4f179bc761388852e44638298
LuisFelipeMod <luisfelipetecno@gmail.com>
2025-03-29 23:05:34 -0300

    feat: add redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <main>
-      
-    </main>
-  );
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/auth/login");
+  } else{
+    redirect("/dashboard");
+  }
 }
