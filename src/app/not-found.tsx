bc7d800b02c57a5af91614382618b8aa3c413a39
LuisFelipeMod <luisfelipetecno@gmail.com>
2025-03-29 23:14:02 -0300

    feat: not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function notFound() {
+  return (
+    <>
+      <section className="flex justify-center items-center h-screen">
+        <h1>Essa página não existe!</h1>
+      </section>
+    </>
+  );
+}
